2c18d74e10c2298124e14c9ebd292762fd225193
Sarecardusk <207292960+Sarecardusk@users.noreply.github.com>
2026-04-10 19:14:49 +0800

    chore(alias): src 工作区别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// @ts-expect-error process is a nodejs global
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
